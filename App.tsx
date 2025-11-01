1f4173c2013a20b2cde4d059001528626e65e815
TarekAbdo223 <tarekk.abdo22@gmail.com>
2025-11-01 19:37:21 +0200

    homeScreen componant start

--- App.tsx
+++ App.tsx
@@ -2,9 +2,10 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import IntroScreen from "./src/screens/IntroScreen";
 import ContactUs from "./src/screens/ContactUs";
+import HomeScreen from "./src/screens/HomeScreen";
 
 export default function App() {
-  return <ContactUs />;
+  return <HomeScreen />;
 }
 
 const styles = StyleSheet.create({
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,38 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { s, vs } from "react-native-size-matters";
+
+const HomeScreen = () => {
+  return (
+    <View
+      style={{
+        paddingTop: vs(50),
+        paddingHorizontal: s(16),
+      }}
+    >
+      <Text
+        style={{
+          color: "#1D150F",
+          fontSize: s(20),
+          fontWeight: "semibold",
+          marginBottom: vs(6),
+        }}
+      >
+        Meditation
+      </Text>
+      <Text
+        style={{
+          color: "#2C2016",
+          marginBottom: vs(16),
+          fontSize: s(14),
+        }}
+      >
+        Lorem Ipsum is simply dummy text
+      </Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
