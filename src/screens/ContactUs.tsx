d4bb68aaf60de839c35ce608e01c7e2e444157f5
TarekAbdo223 <tarekk.abdo22@gmail.com>
2025-11-01 11:58:34 +0200

    add the other texts and the other socail media platforms

--- src/screens/ContactUs.tsx
+++ src/screens/ContactUs.tsx
@@ -9,14 +9,20 @@ import SocialScreen from "../components/SocialScreen";
 
 const ContactUs = () => {
   return (
-    <View
-      style={{ marginTop: vs(50), height: vs(50), paddingHorizontal: s(17) }}
-    >
+    <View style={{ marginTop: vs(50), paddingHorizontal: s(17) }}>
       <View style={styles.header}>
         <BackButton />
         <UserAvatar />
       </View>
-      <SocialScreen />
+      <Text style={styles.contactText}>Contact Us</Text>
+      <View style={styles.socialContainer}>
+        <Text style={styles.socialText}>Social Media Platforms</Text>
+        <SocialScreen />
+        <SocialScreen />
+        <SocialScreen />
+        <SocialScreen />
+        <SocialScreen />
+      </View>
     </View>
   );
 };
@@ -27,5 +33,23 @@ const styles = StyleSheet.create({
   header: {
     flexDirection: "row",
     justifyContent: "space-between",
+  },
+  socialContainer: {
+    backgroundColor: "#F5F5FA",
+    borderRadius: s(14),
+    paddingHorizontal: s(18),
+    paddingVertical: s(15),
+    marginVertical: s(18),
+  },
+  socialText: {
+    color: "#000000",
+    fontWeight: "600",
+    fontSize: s(16),
+  },
+  contactText: {
+    fontWeight: "600",
+    fontSize: s(30),
+    marginStart: s(19),
+    marginTop: s(20),
   },
 });
