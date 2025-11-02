39f1795a42d367b9a706ef402641b3a51c4f029b
TarekAbdo223 <tarekk.abdo22@gmail.com>
2025-11-03 01:05:43 +0200

    creates the meditation component

---
+++ src/components/MeditationCards.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const MeditationCards = () => {
+  return (
+    <View>
+      <Text>MeditationCards</Text>
+    </View>
+  );
+};
+
+export default MeditationCards;
+
+const styles = StyleSheet.create({});
